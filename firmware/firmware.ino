e43d5991120746380b7f869299bab532773d8a32
Emmanuelprime <emmanuelprime2@gmail.com>
2024-07-15 15:31:20 +0100

    plotted odometry in python

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,5 +1,5 @@
-
 #include <math.h>
+
 #define LEFT_ENCODER_A 23
 #define LEFT_ENCODER_B 22
 #define RIGHT_ENCODER_A 21
@@ -13,25 +13,23 @@
 #define L_dir_1_pin 33
 #define L_dir_2_pin 25
 
-float wheel_diameter = 8.4; // centimeters
+float wheel_diameter = 8.4; // meters
 
 long int left_count = 0;
 long int left_count_spd = 0;
 long int right_count = 0;
 long int right_count_spd = 0;
 
-
 int left_encoder_CPR = 207;
 int right_encoder_CPR = 207;
-int desired_count = 3;
-
+int desired_count = 1;
 
 // Odometry variables
 long int del_right_count = 0;
 long int last_right_count = 0;
 long int del_left_count = 0;
 long int last_left_count = 0;
-float wheel_seperation = 0.316; // centimeters
+float wheel_seperation = 31.6; // centimeters
 float left_wheel_distance = 0;
 float right_wheel_distance = 0;
 float robot_distance = 0;
@@ -42,7 +40,7 @@ float phi = 0;
 volatile unsigned current_time = 0;
 volatile unsigned previous_time = 0;
 volatile unsigned delta_time = 0;
-volatile unsigned sample_time = 10; /// in milliseconds
+volatile unsigned sample_time = 100; // in milliseconds
 
 volatile unsigned L_enc_cur_time = 0;
 volatile unsigned L_enc_prev_time = 0;
@@ -50,7 +48,6 @@ volatile unsigned L_enc_del_time = 0;
 double freq_l = 0;
 double Wl = 0;
 double Vl = 0;
-
 
 volatile unsigned R_enc_cur_time = 0;
 volatile unsigned R_enc_prev_time = 0;
@@ -67,9 +64,9 @@ int rightIndex = 0;
 double leftVelocitySum = 0;
 double rightVelocitySum = 0;
 
-
 void setup() {
   Serial.begin(115200);
+
   pinMode(R_speed_pin, OUTPUT);
   pinMode(R_dir_1_pin, OUTPUT);
   pinMode(R_dir_2_pin, OUTPUT);
@@ -78,10 +75,10 @@ void setup() {
   pinMode(L_dir_1_pin, OUTPUT);
   pinMode(L_dir_2_pin, OUTPUT);
   
-  pinMode(LEFT_ENCODER_A,INPUT);
-  pinMode(LEFT_ENCODER_B,INPUT);
-  pinMode(RIGHT_ENCODER_A,INPUT);
-  pinMode(RIGHT_ENCODER_B,INPUT);
+  pinMode(LEFT_ENCODER_A, INPUT);
+  pinMode(LEFT_ENCODER_B, INPUT);
+  pinMode(RIGHT_ENCODER_A, INPUT);
+  pinMode(RIGHT_ENCODER_B, INPUT);
   
   attachInterrupt(digitalPinToInterrupt(LEFT_ENCODER_A), do_left_motor, RISING);
   attachInterrupt(digitalPinToInterrupt(LEFT_ENCODER_B), do_left_motor, CHANGE);
@@ -92,6 +89,7 @@ void setup() {
     leftVelocities[i] = 0;
     rightVelocities[i] = 0;
   }
+
   delay(5000);
 }
 
@@ -101,107 +99,97 @@ void loop() {
   L_enc_cur_time = millis();
   delta_time = current_time - previous_time;
 
-  if(delta_time >= sample_time)
-  {
+  if (delta_time >= sample_time) {
     R_enc_del_time = R_enc_cur_time - R_enc_prev_time;
     L_enc_del_time = L_enc_cur_time - L_enc_prev_time;
-    // compute wheel velocities
-    Wl = desired_count*(2 * PI * freq_l)/ left_encoder_CPR;
-    Vl = Wl * wheel_diameter/2;
-    Wr = desired_count*(2 * PI * freq_r)/ right_encoder_CPR;
-    Vr = Wr * wheel_diameter/2;
-    MoveMotor_R(100,1);
-    MoveMotor_L(100,1);
-    
+
+    // Compute wheel velocities
+    Wl = desired_count * (2 * PI * freq_l) / left_encoder_CPR;
+    Vl = Wl * wheel_diameter / 2;
+    Wr = desired_count * (2 * PI * freq_r) / right_encoder_CPR;
+    Vr = Wr * wheel_diameter / 2;
+
+    MoveMotor_R(120, 1);
+    MoveMotor_L(120, 1);
+
     Odometry();
-    
-//    sendFilteredVelocity();
-    SendOdometry();
+    sendOdometry();
     previous_time = current_time;
   }
   
 }
 
-void sendFilteredVelocity()
-{
+void sendFilteredVelocity() {
   double filteredVl = movingAverageFilter(Vl, leftVelocities, leftIndex, leftVelocitySum);
   double filteredVr = movingAverageFilter(Vr, rightVelocities, rightIndex, rightVelocitySum);
   Serial.print(filteredVr);
   Serial.print(",");
   Serial.println(filteredVl);
 }
-void sendRawVelocity()
-{
+
+void sendRawVelocity() {
   Serial.print(Vr);
   Serial.print(",");
   Serial.println(Vl);
 }
 
-void do_left_motor()
-{
+void sendOdometry() {
+  Serial.print(x);
+  Serial.print(",");
+  Serial.print(y);
+//  Serial.println(y);
+  Serial.print(",");
+  Serial.println(phi);
+}
+
+void do_left_motor() {
   int b = digitalRead(LEFT_ENCODER_B);
-  if(b > 0)
-  {
+  if (b > 0) {
     left_count--;
     left_count_spd--;
-    if(abs(left_count_spd) == desired_count)
-    {
-         if(L_enc_del_time>0)
-        {
-          freq_l = (1000)/(double) L_enc_del_time;
-        }
+    if (abs(left_count_spd) == desired_count) {
+      if (L_enc_del_time > 0) {
+        freq_l = (1000) / (double)L_enc_del_time;
+      }
       left_count_spd = 0;
       L_enc_prev_time = L_enc_cur_time;
     }
-  }
-  else
-  {
+  } else {
     left_count++;
     left_count_spd++;
-    if(abs(left_count_spd) == desired_count)
-    {
-         if(L_enc_del_time>0)
-        {
-          freq_l = (1000)/(double) L_enc_del_time;
-        }
+    if (abs(left_count_spd) == desired_count) {
+      if (L_enc_del_time > 0) {
+        freq_l = (1000) / (double)L_enc_del_time;
+      }
       left_count_spd = 0;
       L_enc_prev_time = L_enc_cur_time;
     }
   }
 }
 
-void do_right_motor()
-{
+void do_right_motor() {
   int b = digitalRead(RIGHT_ENCODER_B);
-  if(b > 0)
-  {
+  if (b > 0) {
     right_count++;
     right_count_spd++;
-    if(abs(right_count_spd) == desired_count)
-    {
-         if(R_enc_del_time>0)
-        {
-          freq_r = (1000)/(double) R_enc_del_time;
-        }
+    if (abs(right_count_spd) == desired_count) {
+      if (R_enc_del_time > 0) {
+        freq_r = (1000) / (double)R_enc_del_time;
+      }
       right_count_spd = 0;
       R_enc_prev_time = R_enc_cur_time;
     }
-  }
-  else
-  {
+  } else {
     right_count--;
     right_count_spd--;
-    if(abs(right_count_spd) == desired_count)
-    {
-         if(R_enc_del_time>0)
-        {
-          freq_r = (1000)/(double) R_enc_del_time;
-        }
+    if (abs(right_count_spd) == desired_count) {
+      if (R_enc_del_time > 0) {
+        freq_r = (1000) / (double)R_enc_del_time;
+      }
       right_count_spd = 0;
       R_enc_prev_time = R_enc_cur_time;
     }
   }
- 
 }
 
 void MoveMotor_L(int speed, int dir) {
@@ -241,28 +229,21 @@ double movingAverageFilter(double newValue, double* values, int& currentIndex, d
   return average;
 }
 
-void Odometry()
-{
-  del_right_count =  right_count - last_right_count;
-  del_left_count =  left_count - last_left_count;
-  right_wheel_distance = (del_right_count * PI * wheel_diameter)/(double)right_encoder_CPR;
-  left_wheel_distance = (del_left_count * PI * wheel_diameter)/(double)left_encoder_CPR;
+void Odometry() {
+  del_right_count = right_count - last_right_count;
+  del_left_count = left_count - last_left_count;
+  right_wheel_distance = (del_right_count * PI * wheel_diameter) / (double)right_encoder_CPR;
+  left_wheel_distance = (del_left_count * PI * wheel_diameter) / (double)left_encoder_CPR;
 
-  robot_distance = (right_wheel_distance + left_wheel_distance)/2;
+  robot_distance = (right_wheel_distance + left_wheel_distance) / 2;
 
-  x = x + robot_distance*cos(phi);
-  y = y + robot_distance*sin(phi);
+  x = x + robot_distance * cos(phi);
+  y = y + robot_distance * sin(phi);
 
-  phi = phi + (right_wheel_distance - left_wheel_distance)/wheel_seperation;
-  phi = atan2(sin(phi),cos(phi));  // keep phi between pi and -pi 
+  phi = phi + (right_wheel_distance - left_wheel_distance) / wheel_seperation;
+  phi = atan2(sin(phi), cos(phi)); // keep phi between pi and -pi 
+  
 
   last_right_count = right_count;
   last_left_count = left_count;
-}
-
-void SendOdometry()
-{
-  Serial.print(x);
-  Serial.print(",");
-  Serial.println(y);
 }
---
+++ plot_odometry.py
@@ -0,0 +1,36 @@
+import serial
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+
+x_pos = []
+y_pos = []
+
+def update(frame):
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').strip()
+        try:
+            x, y, phi = map(float, line.split(','))
+            x_pos.append(x)
+            y_pos.append(-y)
+            
+            ax.clear()
+            ax.plot(y_pos, x_pos, 'bo-', label='Path')
+            ax.plot(y_pos[-1], x_pos[-1], 'ro', label='Current Position') 
+            ax.set_title('Robot Path')
+            ax.set_xlabel('Y Position (m)')
+            ax.set_ylabel('X Position (m)')
+            ax.legend()
+            ax.axis('equal') 
+
+        except ValueError:
+            print(f"Could not parse line: {line}")
+
+fig, ax = plt.subplots()
+fig.suptitle('Odometry Data')
+
+ani = FuncAnimation(fig, update, interval=10)
+
+plt.show()
+ser.close()
